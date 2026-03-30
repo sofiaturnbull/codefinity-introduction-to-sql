245eb9e15e9b67e72ede1ba09b9bf548d8b6a316
sofiaturnbull <>
2026-03-30 14:50:36 +0000

    Filter chaining

---
+++ section/filter_chaining/query.sql
@@ -0,0 +1,4 @@
+-- Write a SQL query to select only the names of countries from Asia with a population greater than 1000000
+Select name
+From country
+Where continent= 'Asia' and population >1000000
